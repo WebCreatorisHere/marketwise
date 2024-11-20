ce900d14e8a0489b2de26d0d9d9f35c416af81a2
Yash <pratibhad.amit@gmail.com>
2024-11-20 14:26:03 +0530

    aa

--- app/db/connector.js
+++ app/db/connector.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectdb = async()=>{
     try {
-       await mongoose.connect(`${process.env.MONGO_URI}`, {
+       await mongoose.connect("mongodb+srv://yashthecool:aajbluehaipanipani@cluster12.wd89a.mongodb.net/marketwise", {
           useNewUrlParser: true,
         });
       } catch (error) {
