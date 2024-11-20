ea05207a12b6905eebe189158945043066e6e1a3
Yash <pratibhad.amit@gmail.com>
2024-11-20 23:17:01 +0530

    finale

--- app/db/connector.js
+++ app/db/connector.js
@@ -6,7 +6,7 @@ const connectdb = async () => {
     return;
   }
 
-  const uri = process.env.NEXT_PUBLIC_MONGODB_URL;
+  const uri = process.env.MONGODB_URI;
   if (!uri) {
     console.error("MongoDB URI is undefined. Please check your environment variables.");
     return;
